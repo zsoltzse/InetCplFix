4fb4a49ceee839248be36c58ef9a609537884a05
zsoltzse <51295626+zsoltzse@users.noreply.github.com>
2021-03-24 14:20:34 +0100

    Add files via upload

---
+++ InetCplFix.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+
+ECHO **********************************************************************
+ECHO *                                                                    *
+ECHO *                          Made by: Zsolesz                          *
+ECHO * If you have any question you can find me in the darkbot DC server. *
+ECHO *               https://darkbot.eu/ --- Discord Server               *
+ECHO *                                                                    *
+ECHO **********************************************************************
+PAUSE
+ 
+start cmd /c "Rundll32.exe InetCpl.cpl,ClearMyTracksByProcess 8"
+
+EXIT
